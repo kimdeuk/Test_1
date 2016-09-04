b28fe79938edea82167cc1ba731f90a2db231852
kimdeuk <bellz17@naver.com>
2016-09-04 17:23:36 +0900

    별로 수정한 건 없습니다. 낙서 한번 해봤습니다.

--- app/src/main/java/com/example/user/test_1/Test_1Activity.java
+++ app/src/main/java/com/example/user/test_1/Test_1Activity.java
@@ -1,15 +1,30 @@
 package com.example.user.test_1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Test_1Activity extends AppCompatActivity {
+    Button btn01;
+    Button btn02;
+    Button btn03;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("김원득의 프로젝트");
         setContentView(R.layout.activity_test_1);
 
-        setTitle("김원득의 프로젝트");
+        btn01 = (Button) findViewById(R.id.btn01);
+        btn01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Test_1Activity.this,NaxtActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
